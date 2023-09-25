26622dcf0e1b86b176765080db409f18c295a0e2
Martijn Hiemstra <info@greenhighway.nl>
2023-09-25 14:59:52 +0200

    Implemented login

--- js/login-page.js
+++ js/login-page.js
@@ -4,13 +4,31 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "furkan" && password === "password") {
-        alert("You have successfully logged in.");
-        window.location = "receptentabelfrontend.html"
-    } else {
-        loginErrorMsg.style.opacity = 1;
+    let dto = {
+        "email": username,
+        "password": password,
     }
+
+    fetch(db + '/gebruiker/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dto)
+    })
+        .then(response => response.json()) 
+        .then(success => {
+            if (success) {
+                loginErrorMsg.style.opacity = 0;
+
+                alert("You have successfully logged in.");
+                window.location = "receptentabelfrontend.html"
+            } else {
+                loginErrorMsg.style.opacity = 1;
+            }
+        })
 })
